78c6305841d9b3aab1bc916ef27a37b9ba8529c7
Shem <sgyll@protonmail.com>
2019-07-22 23:07:32 +0100

    Cleeeeeeaaaaan

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c6d12e1e12c24658efcba159a9a4d6ce.js"
+  "/precache-manifest.d3e5ad34747a2357304f2e6772654e2d.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/App.js
+++ src/App.js
@@ -23,48 +23,58 @@ class App extends Component {
       <div className="container">
         <div className="col-xs-12">
           <img src={logo} className="w-50 m-3" alt="Smashesque"></img>
-          <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Search a Title, Platform or Tag" aria-label="Search a Title, Platform or Tag" aria-describedby="Search a Title, Platform or Tag"></input>
-            <div class="input-group-append">
-              <button class="btn btn-outline-secondary" type="button"><img src={search} alt="Search"></img></button>
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Search a Title, Platform or Tag" aria-label="Search a Title, Platform or Tag" aria-describedby="Search a Title, Platform or Tag"></input>
+            <div className="input-group-append">
+              <button className="btn btn-outline-secondary" type="button"><img src={search} alt="Search"></img></button>
             </div>
           </div>
         </div>
+
         {this.state.games.map((game) => (
-          <div className="card mb-3">
+          <div className="card border-0">
             <div className="row">
-              <div className="col-5">
-                <div className="card-body">
+              <div className="col-1">
+                <img src={game.fields.Image[0].thumbnails.large.url} alt={game.fields.Name} width="100px" className=""></img>
+              </div>
+              <div className="col">
+                <div className="card-body pt-0">
                   <h5 className="card-title">{game.fields.Name} <span className="text-muted">{game.fields.Type}</span></h5>
                   <p>{game.fields.Description}</p>
+                  <div className="row">
+                    <div className="col-auto"><strong>Max Players: </strong>
+                    <p>{game.fields.MaxPlayers}</p></div>
+                    <div className="col-auto"><strong>Roster size: </strong><p>{game.fields.Roster === 0 ? 'Other' : game.fields.Roster}</p></div>
+                    <div className="col-auto"><h6>Platforms:</h6>
+                      
+                        {game.fields.Platforms.map((element) => (
+                          <button className="btn btn-outline-info btn-sm m-1">{element} </button>
+                        ))}
+                      
+                      </div>
+
+                      <div className="col-auto"><h6>Tags: </h6>
+                      
+                        {game.fields.Tags.map((element) => (
+                          <button className="btn btn-outline-success btn-sm m-1">{element} </button>
+                        ))}
+                      
+                    </div>
+                  </div>
+                  </div>
+                  <div className="row">
+                    <div className="col pt-3">
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div className="col-4 pt-3">
-                <p><strong>Max Players: </strong>{game.fields.MaxPlayers}</p>
-                <p><strong>Roster size: </strong>{game.fields.Roster === 0 ? 'Other' : game.fields.Roster}</p>
-                <h6>Platforms:</h6>
-                <p>
-                  {game.fields.Platforms.map((element) => (
-                    <button className="btn btn-outline-info btn-sm m-1">{element} </button>
-                  ))}
-                </p>
-                <h6>Tags: </h6>
-                <p>
-                  {game.fields.Tags.map((element) => (
-                    <button className="btn btn-outline-success btn-sm m-1">{element} </button>
-                  ))}
-                </p>
-              </div>
-              <div className="col-1">
-                <img src={game.fields.Image[0].thumbnails.large.url} alt={game.fields.Name} width="250px" className="p-3"></img>
               </div>
             </div>
 
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
 
+            ))}
+      </div>
+        );
+        }
+      }
+      
 export default App;
